4082bf772baf083e6c11d85a5ee5a0f2b5e97907
Артём Ермилов <ermilov-artjom@rambler.ru>
2022-06-13 23:00:32 +0300

    Решение задачи 4

---
+++ Example003/Program.cs
@@ -0,0 +1,53 @@
+﻿//Задача 3: Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры
+// нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+string aString = Console.ReadLine();
+int aInt = int.Parse(aString);
+
+if (aInt < 100)
+{
+    Console.WriteLine($"Вы ввели число {aInt}, у которого нет 3-й цифры ");
+}
+
+else
+
+{
+    int contri = 10;
+    int begin = aInt;
+    double summ = 0;
+    int div = 1;
+    double x = 10;
+
+    while (begin != 0) 
+    {
+        begin = aInt / contri;
+        contri = contri * 10;
+        summ++;
+        
+    }
+
+    
+
+    int sumWh1 = 0;
+    int c1 = aInt;
+    int c2 = 0;
+
+    while (sumWh1 < 3)
+    {
+        div = Convert.ToInt32(Math.Pow(x, summ - 1));
+        c2 = c1 / div;
+        c1 = c1 - (c2 * div);
+
+        sumWh1++;
+        summ--;
+
+    }
+
+
+    Console.WriteLine($"В числе {aInt} 3-я цифра равняется {c2}");
+}
---
+++ Example004/Program.cs
@@ -0,0 +1,29 @@
+﻿// адача 4: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите день недели в виде числа от 1 до 7: ");
+string aString = Console.ReadLine();
+int aInt = int.Parse(aString);
+
+while (!(0<aInt)||aInt>7)
+{
+    Console.Write("Вы ввели не то число,  введите число от 1 до 7 заново: ");
+    string a1String = Console.ReadLine();
+    int a1Int = int.Parse(a1String);
+    aInt=a1Int;
+}
+
+if (aInt<=5)
+{
+  Console.WriteLine($"День недели под номером {aInt} не является выходным. ");  
+}
+else
+{
+    Console.WriteLine($"День недели под номером {aInt} является выходным. ");
+}
+
+
