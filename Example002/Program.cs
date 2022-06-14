5ef091ac9833d590ccbc519cd989ad86cdf6a3f9
Artem Ermilov <ermilo-artjom@rambler.ru>
2022-06-14 18:03:01 +0300

    Изменения в задаче 2

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -9,6 +9,19 @@
 
 int aInt = new Random().Next(100,1000);
 
-int result = ((aInt/100)*10)+(aInt-((aInt/10)*10));
+int aInt1 = aInt;
 
-Console.WriteLine($"Из числа {aInt}  при удалении 2-е цифры получается число  {result} ");
+int[] array = new int[3];
+int index = 2;
+
+while (aInt>0)
+{
+   array[index] = aInt % 10;
+   aInt=aInt/10;
+   index--;  
+}
+
+int result=array[0]*10+array[2];
+//int result = ((aInt/100*10)+aInt%10);
+
+Console.WriteLine($"Из числа {aInt1}  при удалении 2-е цифры получается число  {result} ");
